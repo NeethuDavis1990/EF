a4916649afe02cfb1b9d969e428eeb41d041d21f
NeethuDavis1990 <86987322+NeethuDavis1990@users.noreply.github.com>
2022-01-25 22:32:22 +0100

    categorycreate

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -20,5 +20,49 @@ namespace SampleNewProject.Controllers
             IEnumerable<Category> objCategoryList = _db.Categories;
             return View(objCategoryList);
         }
+        //GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            //Custom validations on the server side
+            if(obj.Name ==obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Error", "Both values should not be same");
+            }
+            if (ModelState.IsValid)
+            {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        //GET
+        public IActionResult Edit(int? id)
+        {
+            if(id==null || id == 0)
+            {
+                return NotFound();
+            }
+            var Categoryfromdb = _db.Categories.Find(id);
+            //var Categoryfromdb = _db.Categories.SingleOrDefault(x=>x.Id==id);
+            if (Categoryfromdb == null)
+            {
+                return NotFound();
+            }
+            return View(Categoryfromdb);
+        }
+
+        public IActionResult Edit()
+        {
+            return View();
+        }
     }
 }
--- Models/Category.cs
+++ Models/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,6 +12,8 @@ namespace SampleNewProject.Models
         [Key]
         public int Id { get; set; }
         [Required]
+        [DisplayName ("Category Name")]
+        [Range (4,25,ErrorMessage ="Range should be between 4 and 25 characters")]
         public string Name { get; set; }
         public int DisplayOrder { get; set; }
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
